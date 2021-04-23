431d415fcc0c3d859b0f8c92b6b633def94a0b16
raimonestanyol <raimon.estanol@in2.es>
2021-04-23 02:44:06 +0200

    final version (with debug on)

--- project/project/settings.py
+++ project/project/settings.py
@@ -124,6 +124,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
